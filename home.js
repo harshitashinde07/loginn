238ad54b3e6ada8c824a8a5437e392cd630d38b1
harshitashinde07 <56025707+harshitashinde07@users.noreply.github.com>
2020-08-08 16:40:17 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,31 @@
+/*function validate()
+{
+      var username=document.getElementById('user').value;
+      if(username.trim()=='')
+      {
+        alert("invalid");
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+}*/
+/*(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();*/
+
